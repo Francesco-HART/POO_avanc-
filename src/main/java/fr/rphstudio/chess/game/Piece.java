5412ecc3c0df2b9629c397cf7123f5b26b8de6ae
Debuchy <florentdebuchy@orange.fr>
2020-02-13 09:23:03 +0100

    add chrono

--- src/main/java/fr/rphstudio/chess/game/Piece.java
+++ src/main/java/fr/rphstudio/chess/game/Piece.java
@@ -4,6 +4,8 @@ import fr.rphstudio.chess.interf.IChess;
 import fr.rphstudio.chess.interf.IMove;
 import fr.rphstudio.chess.pieces.*;
 
+import javax.swing.plaf.nimbus.State;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Piece
@@ -11,6 +13,7 @@ public class Piece
     private IChess.ChessColor color;
     private IChess.ChessType type;
     private IMove iMove;
+    private int nbMoves = 0;
 
     public Piece(IChess.ChessColor color, IChess.ChessType type , IMove moves)
     {
@@ -29,8 +32,13 @@ public class Piece
         return this.type;
     }
 
-    public List<IChess.ChessPosition> getMoves(IChess.ChessPosition p, Board brd){
+    public List<IChess.ChessPosition> getMoves(IChess.ChessPosition p, Board brd)
+    {
        return iMove.move(p, brd);
+    }
 
+
+    public int countMoves(){
+        return nbMoves++;
     }
 }
